72ab5f8aaad102d1a8238777011e53d1d96a2fbe
unknown <andresbasilealvarez@gmail.com>
2021-10-07 11:43:56 -0500

    separación en TABS para cada algoritmo

--- main.py
+++ main.py
@@ -1,33 +1,15 @@
 import streamlit as st
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-from apyori import apriori
+import MetricasDistancia
+import ReglasAsociacion
+
+TABS = {"MetricasDistancia": MetricasDistancia, "ReglasAsociacion": ReglasAsociacion}
+
+st.sidebar.title('Algoritmos')
+selection = st.sidebar.radio("Ir a", list(TABS.keys()))
+page = TABS[selection]
+page.app()
 
 
-st.title('InteliShow')
 
-DatosPeliculas = pd.read_csv('movies.csv', header=None)
-st.write(DatosPeliculas.head(10))
 
-Transacciones = DatosPeliculas.values.reshape(-1).tolist()
-#Transacciones
 
-Lista = pd.DataFrame(Transacciones)
-Lista['Frecuencia'] = 1  # valor que después se reemplazará, es nada más para agregar la columna. 
-
-#Agrupamos los elementos
-#Conteo
-# By=[0] empieza en cero.
-# as_index para que no aparezca el nombre 
-Lista = Lista.groupby(by=[0], as_index=False).count().sort_values(by=['Frecuencia'], ascending=True)
-Lista['Porcentaje'] = (Lista['Frecuencia']/Lista['Frecuencia'].sum())
-Lista = Lista.rename(columns={0:'Item'})
-
-Lista #vemos que el máximo de apariciones en porc
-
-fig = plt.figure(figsize=(16,20), dpi = 300)
-plt.ylabel('Item')
-plt.xlabel('Frecuencia')
-plt.barh(Lista['Item'], width=Lista['Frecuencia'], color='#FF2442')
-st.pyplot(fig)
